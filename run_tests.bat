1fd135ecde46e66e8261b8f86324740d77e525ff
Toxantron <thomas.fuchs91@gmail.com>
2016-04-20 22:36:18 +0200

    Coverage reports

---
+++ run_tests.bat
@@ -0,0 +1,5 @@
+# Run tests with open cover
+packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -filter:"+[*]*" -target:"nunit-console-x86 /noshadow /domain:single CGbR.Tests\bin\Release\CGbR.Tests.dll CGbR.GeneratorTests\bin\Release\CGbR.GeneratorTests.dll" -output:coverage.xml
+
+# Publish coverage report
+packages\coveralls.io.1.3.4\tools\coveralls.net.exe --opencover coverage.xml
