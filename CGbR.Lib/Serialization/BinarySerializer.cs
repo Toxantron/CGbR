80c45afbc18c01e583292382b1a6eea04650056a
Toxantron <thomas.fuchs91@gmail.com>
2016-04-25 17:18:16 +0200

    BinarySerializer added

---
+++ CGbR.Lib/Serialization/BinarySerializer.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CGbR.Lib
+{
+    /// <summary>
+    /// Helper class to serializer and deserialize objects to byte arrays
+    /// </summary>
+    public static class BinarySerializer
+    {
+        /// <summary>
+        /// Serialize object to byte array
+        /// </summary>
+        /// <param name="instance">Instance to serialize</param>
+        /// <returns>Bytes containing the object properties</returns>
+        public static byte[] Serialize(IByteSerializable instance)
+        {
+            var index = 0;
+            var bytes = new byte[instance.Size];
+            instance.ToBytes(bytes, ref index);
+            return bytes;
+        }
+
+        /// <summary>
+        /// Serialize a collection of objects to byte array
+        /// </summary>
+        /// <param name="instances">Instance to serialize</param>
+        /// <returns>Bytes containing the object properties</returns>
+        public static byte[] SerializeMany(ICollection<IByteSerializable> instances)
+        {
+            var index = 0;
+            var bytes = new byte[instances.Sum(i => i.Size)];
+            foreach (var instance in instances)
+            {
+                instance.ToBytes(bytes, ref index);
+            }
+            return bytes;
+        }
+
+        /// <summary>
+        /// Deserialize object from byte array
+        /// </summary>
+        /// <typeparam name="T">Type of object to deserialize</typeparam>
+        /// <param name="bytes">Byte array containing property values</param>
+        /// <returns>Object instance</returns>
+        public static T Deserialize<T>(byte[] bytes)
+            where T : IByteSerializable, new()
+        {
+            var index = 0;
+            var instance = new T();
+            instance.FromBytes(bytes, ref index);
+            return instance;
+        }
+
+        /// <summary>
+        /// Deserialize object from byte array
+        /// </summary>
+        /// <typeparam name="T">Type of object to deserialize</typeparam>
+        /// <param name="bytes">Byte array containing property values</param>
+        /// <returns>Object instance</returns>
+        public static ICollection<T> DeserializeMany<T>(byte[] bytes)
+            where T : IByteSerializable, new()
+        {
+            var index = 0;
+            var instances = new List<T>();
+            while (index < bytes.Length)
+            {
+                var instance = new T();
+                instance.FromBytes(bytes, ref index);
+                instances.Add(instance);
+            }
+            return instances;
+        }
+    }
+}
---
+++ CGbR.Lib/Serialization/IByteSerializable.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace CGbR.Lib
+{
+    /// <summary>
+    /// Instances of this class have generated methods for binary serialization and deserialization
+    /// </summary>
+    public interface IByteSerializable
+    {
+        /// <summary>
+        /// Binary size of this object and its children
+        /// </summary>
+        int Size { get; }
+
+        /// <summary>
+        /// Write object to the given byte array
+        /// </summary>
+        /// <param name="bytes">Target array</param>
+        /// <param name="index">Position in the array</param>
+        /// <returns></returns>
+        void ToBytes(byte[] bytes, ref int index);
+
+        /// <summary>
+        /// Read object properties from bytes
+        /// </summary>
+        /// <param name="bytes">Serialized array of bytes</param>
+        /// <param name="index">Current position in array</param>
+        void FromBytes(byte[] bytes, ref int index);
+    }
+}
