b98c3e275f5e4c9426820918f05c610b82bc9046
Thomas Fuchs <thomas.fuchs91@gmail.com>
2016-04-28 11:16:15 +0200

    Started to provide methods for #13

--- CGbR.Lib/Tools/GeneratorByteConverter.cs
+++ CGbR.Lib/Tools/GeneratorByteConverter.cs
@@ -19,64 +19,64 @@ namespace CGbR.Lib
         /// </summary>
         public static unsafe void Include(short value, byte[] bytes, int index)
         {
-            fixed (byte* b = bytes)
-                *((short*)(b + index)) = value;
+            fixed (byte* b = &bytes[index])
+                *((short*)b) = value;
         }
         /// <summary>
         /// Writer property of type UInt16 to bytes by using pointer opertations
         /// </summary>
         public static unsafe void Include(ushort value, byte[] bytes, int index)
         {
-            fixed (byte* b = bytes)
-                *((ushort*)(b + index)) = value;
+            fixed (byte* b = &bytes[index])
+                *((ushort*)b) = value;
         }
         /// <summary>
         /// Writer property of type Int32 to bytes by using pointer opertations
         /// </summary>
         public static unsafe void Include(int value, byte[] bytes, int index)
         {
-            fixed (byte* b = bytes)
-                *((int*)(b + index)) = value;
+            fixed (byte* b = &bytes[index])
+                *((int*)b) = value;
         }
         /// <summary>
         /// Writer property of type UInt32 to bytes by using pointer opertations
         /// </summary>
         public static unsafe void Include(uint value, byte[] bytes, int index)
         {
-            fixed (byte* b = bytes)
-                *((uint*)(b + index)) = value;
+            fixed (byte* b = &bytes[index])
+                *((uint*)b) = value;
         }
         /// <summary>
         /// Writer property of type Single to bytes by using pointer opertations
         /// </summary>
         public static unsafe void Include(float value, byte[] bytes, int index)
         {
-            fixed (byte* b = bytes)
-                *((float*)(b + index)) = value;
+            fixed (byte* b = &bytes[index])
+                *((float*)b) = value;
         }
         /// <summary>
         /// Writer property of type Double to bytes by using pointer opertations
         /// </summary>
         public static unsafe void Include(double value, byte[] bytes, int index)
         {
-            fixed (byte* b = bytes)
-                *((double*)(b + index)) = value;
+            fixed (byte* b = &bytes[index])
+                *((double*)b) = value;
         }
         /// <summary>
         /// Writer property of type Int64 to bytes by using pointer opertations
         /// </summary>
         public static unsafe void Include(long value, byte[] bytes, int index)
         {
-            fixed (byte* b = bytes)
-                *((long*)(b + index)) = value;
+            fixed (byte* b = &bytes[index])
+                *((long*)b) = value;
         }
         /// <summary>
         /// Writer property of type UInt64 to bytes by using pointer opertations
         /// </summary>
         public static unsafe void Include(ulong value, byte[] bytes, int index)
         {
-            fixed (byte* b = bytes)
-                *((ulong*)(b + index)) = value;
+            fixed (byte* b = &bytes[index])
+                *((ulong*)b) = value;
         }
 
         /// <summary>
@@ -91,6 +91,31 @@ namespace CGbR.Lib
 
             Buffer.BlockCopy(Encoder.GetBytes(value), 0, bytes, index, value.Length);
             index += value.Length;
+        }
+        
+        /// <summary>
+        /// Convert bytes at given position to Int16 and increment index
+        /// <summary>
+        public static unsafe short ToInt16(byte[] value, ref int index) 
+        {
+            fixed( byte * pbyte = &value[index]) 
+            {
+                if( index % 2 == 0) 
+                { // data is aligned 
+                    return *((short *) pbyte);
+                }
+                else 
+                {
+                    if( IsLittleEndian) 
+                    { 
+                        return (short)((*pbyte) | (*(pbyte + 1) << 8)) ;
+                    }
+                    else 
+                    {
+                        return (short)((*pbyte << 8) | (*(pbyte + 1)));                        
+                    }
+                }
+            }
         }
 
         /// <summary>
