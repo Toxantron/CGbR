2f1aa5eb66c642ed05efe50e860c0de97ee7862e
Toxantron <thomas.fuchs91@gmail.com>
2016-05-22 09:35:27 +0200

    Bug: Methods read as properties

--- CGbR/Parser/RegexParser.cs
+++ CGbR/Parser/RegexParser.cs
@@ -19,7 +19,7 @@ namespace CGbR
         private readonly Regex _classRegex = new Regex(@" (?<accessModifier>(?:public|internal))(?<isPartial> partial)? class (?<className>\w+)(?: : )?(?<baseType>[A-Z][a-z]\w+)?(?:, )?(?:(?<interface>I\w+)(?:, )?)*");
         private readonly Regex _enumRegex = new Regex(@" (?<accessModifier>(?:public|internal)) enum (?<enumName>\w+)(?: : )?(?<type>\w+)?");
 
-        private readonly Regex _propRegex = new Regex(@" (?<accessModifier>(?:public|internal|private)) (?:(?<collectionType>\w+)<)?(?<type>\w+)(?<isArray>\[(?<dimensions>, ?)*\])?>? (?<name>_?\w+)");
+        private readonly Regex _propRegex = new Regex(@" (?<accessModifier>(?:public|internal|private)) (?:(?<collectionType>\w+)<)?(?<type>\w+)(?<isArray>\[(?<dimensions>, ?)*\])?>? (?<name>_?\w+) ");
         private readonly Regex _memberRegex = new Regex(@" (?<name>\w+)(?: ?= ?)?(?<value>\d+)?");
 
 
