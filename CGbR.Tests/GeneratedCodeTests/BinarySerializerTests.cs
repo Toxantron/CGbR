b44b98c93377accb39f7f26d83bc6e8bd0d24aab
Toxantron <thomas.fuchs91@gmail.com>
2016-07-26 22:50:19 +0200

    First tests migrated

--- CGbR.Tests/GeneratedCodeTests/BinarySerializerTests.cs
+++ CGbR.Tests/GeneratedCodeTests/BinarySerializerTests.cs
@@ -1,49 +1,49 @@
-﻿using NUnit.Framework;
+﻿//using NUnit.Framework;
 
-namespace CGbR.GeneratorTests
-{
-    [TestFixture]
-    public class BinarySerializerTests
-    {
-        [Test]
-        public void EmptyToBytes()
-        {
-            // Arrange
-            var root = new Root(0);
+//namespace CGbR.GeneratorTests
+//{
+//    [TestFixture]
+//    public class BinarySerializerTests
+//    {
+//        [Test]
+//        public void EmptyToBytes()
+//        {
+//            // Arrange
+//            var root = new Root(0);
 
-            // Act
-            var bytes = root.ToBytes();
+//            // Act
+//            var bytes = root.ToBytes();
 
-            // Assert
-            Assert.AreEqual(8, bytes.Length);
-            Assert.IsTrue(BytesEqual(bytes, new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 }));
-        }
+//            // Assert
+//            Assert.AreEqual(8, bytes.Length);
+//            Assert.IsTrue(BytesEqual(bytes, new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 }));
+//        }
 
-        [Test]
-        public void FieldOnly()
-        {
-            // Arrange
-            var root = new Root(10);
+//        [Test]
+//        public void FieldOnly()
+//        {
+//            // Arrange
+//            var root = new Root(10);
 
-            // Act
-            var bytes = root.ToBytes();
+//            // Act
+//            var bytes = root.ToBytes();
 
-            // Assert
-            Assert.AreEqual(8, bytes.Length);
-            Assert.IsTrue(BytesEqual(bytes, new byte[] { 10, 0, 0, 0, 0, 0, 0, 0 }));
-        }
+//            // Assert
+//            Assert.AreEqual(8, bytes.Length);
+//            Assert.IsTrue(BytesEqual(bytes, new byte[] { 10, 0, 0, 0, 0, 0, 0, 0 }));
+//        }
 
-        private static bool BytesEqual(byte[] first, byte[] second)
-        {
-            if (first.Length != second.Length)
-                return false;
+//        private static bool BytesEqual(byte[] first, byte[] second)
+//        {
+//            if (first.Length != second.Length)
+//                return false;
 
-            for (int i = 0; i < first.Length; i++)
-            {
-                if (first[i] != second[i])
-                    return false;
-            }
-            return true;
-        }
-    }
-}
+//            for (int i = 0; i < first.Length; i++)
+//            {
+//                if (first[i] != second[i])
+//                    return false;
+//            }
+//            return true;
+//        }
+//    }
+//}
--- CGbR.Tests/GeneratorTests.cs
+++ CGbR.Tests/GeneratorTests.cs
@@ -1,18 +1,105 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
 
 namespace CGbR.Tests
 {
     [TestFixture]
     public class GeneratorTests
     {
-        [Test]
-        public void GenerateJson()
+        [TestCase(nameof(JsonSerializer), new[]
+        {
+            "System",
+            "System.Collections.Generic",
+            "System.IO",
+            "System.Globalization",
+            "System.Text",
+            "Newtonsoft.Json"
+        })]
+        [TestCase(nameof(BinarySerializer), new[]
+        {
+            "System",
+            "System.Collections.Generic",
+            "System.Linq",
+            "System.Text",
+            "CGbR.Lib"
+        })]
+        [TestCase(nameof(Cloneable), new[]
+        {
+            "System",
+            "System.Collections.Generic",
+        })]
+        public void Usings(string generator, string[] expected)
         {
             // Arrange
-            var model = new ClassModel("JsonDummy")
+            var gen = GetGenerator(generator);
+
+            // Act
+            var interfaces = gen.Usings;
+
+            // Assert
+            Assert.AreEqual(expected.Length, interfaces.Length);
+            foreach (var @interface in expected)
             {
-                AccessModifier = AccessModifier.Public
+                Assert.IsTrue(interfaces.Contains(@interface));
+            }
+        }
+
+        [TestCase(nameof(JsonSerializer), "JsonValue")]
+        [TestCase(nameof(BinarySerializer), "BinaryValue")]
+        [TestCase(nameof(Cloneable), "CloneValue")]
+        public void TestValueTypes(string generator, string expectedCode)
+        {
+            // Arrange
+            var model = GenerateDataContract(AccessModifier.Public);
+            foreach (ModelValueType value in Enum.GetValues(typeof(ModelValueType)))
+            {
+                if(value == ModelValueType.Class)
+                    continue;
+
+                var prop = new PropertyModel($"{value}Prop")
+                {
+                    AccessModifier = AccessModifier.Internal,
+                    ValueType = value,
+                    ElementType = value.ToString("G"),
+                };
+                prop.Attributes.Add(new AttributeModel("DataMember"));
+                model.Properties.Add(prop);
+            }
+
+            // Act
+            var gen = GetGenerator(generator);
+            var generated = gen.Extend(model);
+
+            // Assert by comparing generated fragment with test oracle
+            var expected = File.ReadAllText(Path.Combine("Expected", $"{expectedCode}.txt"));
+            Assert.AreEqual(expected, generated);
+        }
+
+        /// <summary>
+        /// Generate a class model to generate for
+        /// </summary>
+        private static ClassModel GenerateDataContract(AccessModifier access)
+        {
+            var model = new ClassModel("Dummy")
+            {
+                AccessModifier = access,
+                IsPartial = true,
             };
+            model.Attributes.Add(new AttributeModel("DataContract")); // Attributes for JSON and Binary
+            model.Interfaces = new[] { "ICloneable" }; // Cloneable for CloneGenerator
+
+            return model;
+        }
+
+        /// <summary>
+        /// Fetch generator for different causes
+        /// </summary>
+        private static ILocalGenerator GetGenerator(string genName)
+        {
+            return (ILocalGenerator)GeneratorFactory.Resolve(genName);
         }
     }
 }
