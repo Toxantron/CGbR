abbe3b7acaadade50e99bbf42abd899f83db3c0c
Toxantron <thomas.fuchs91@gmail.com>
2016-07-27 00:02:12 +0200

    Size calculation tests

--- CGbR.Tests/GeneratedCodeTests/SizeCalculationTests.cs
+++ CGbR.Tests/GeneratedCodeTests/SizeCalculationTests.cs
@@ -1,48 +1,68 @@
-﻿//using System.Collections.Generic;
-//using NUnit.Framework;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
 
-//namespace CGbR.GeneratorTests
-//{
-//    [TestFixture]
-//    public class SizeCalculationTests
-//    {
-//        [TestCase("", 4)]
-//        [TestCase("Hello world!", 16)]
-//        public void PartialSize(string name, int expectedSize)
-//        {
-//            // Arrange 
-//            var partial = new Partial {Name = name};
+namespace CGbR.GeneratorTests
+{
+    [TestFixture]
+    public class SizeCalculationTests
+    {
+        [TestCase(ModelValueType.Byte, 1)]
+        [TestCase(ModelValueType.Boolean, 1)]
+        [TestCase(ModelValueType.Char, 2)]
+        [TestCase(ModelValueType.Int16, 2)]
+        [TestCase(ModelValueType.UInt16, 2)]
+        [TestCase(ModelValueType.Int32,4)]
+        [TestCase(ModelValueType.UInt32, 4)]
+        [TestCase(ModelValueType.Single, 4)]
+        [TestCase(ModelValueType.Int64, 8)]
+        [TestCase(ModelValueType.UInt64, 8)]
+        [TestCase(ModelValueType.Double, 8)]
+        public void ValueTypeSize(ModelValueType type, int expectedSize)
+        {
+            // Arrange 
+            var model = new ClassModel("Dummy");
+            var prop = new PropertyModel("Property")
+            {
+                ValueType = type,
+                ElementType = type.ToString("G")
+            };
+            prop.Attributes.Add(new AttributeModel("DataMember"));
+            model.Properties.Add(prop);
 
-//            // Act
-//            var size = partial.Size;
+            // Act
+            var size = BinarySize.OfClass(model, new BinarySerializer());
 
-//            // Assert
-//            Assert.AreEqual(expectedSize, size);
-//        }
+            // Assert
+            Assert.AreEqual(expectedSize, size);
+        }
 
-//        [Test]
-//        public void RootSize()
-//        {
-//            // Arrange 
-//            var root = new Root(10)
-//            {
-//                Numbers = new List<ulong> {10, 11, 12},
-//                Partials = new[]
-//                {
-//                    new Partial {Id = 2, Name = "Test"},
-//                    new Partial {Id = 10, Name = "Bob"}
-//                }
-//            };
+        [Test]
+        public void CollectionSizes()
+        {
+            // Arrange
+            var model = new ClassModel("Dummy");
+            var prop = new PropertyModel("Dim1")
+            {
+                IsCollection = true,
+                CollectionType = "List",
+                Dimensions = 1
+            };
+            prop.Attributes.Add(new AttributeModel("DataMember"));
+            model.Properties.Add(prop);
+            prop = new PropertyModel("Dim2")
+            {
+                IsCollection = true,
+                CollectionType = "Array",
+                Dimensions = 2
+            };
+            prop.Attributes.Add(new AttributeModel("DataMember"));
+            model.Properties.Add(prop);
 
-//            // Act
-//            var size = root.Size;
+            // Act
+            var size = BinarySize.OfClass(model, new BinarySerializer());
 
-//            // Assert
-//            var expectedSize = 8;
-//            expectedSize += 24;
-//            expectedSize += 8;
-//            expectedSize += 7;
-//            Assert.AreEqual(expectedSize, size);
-//        }
-//    }
-//}
+            // Assert
+            Assert.AreEqual(6, size);
+        }
+    }
+}
