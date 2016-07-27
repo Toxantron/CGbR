5d93ec28803fb42cbedca3f21ed971fcfd6ed6a6
Toxantron <thomas.fuchs91@gmail.com>
2016-07-27 22:33:43 +0200

    Tests for classes references and binary size

--- CGbR.Tests/GeneratorTests.cs
+++ CGbR.Tests/GeneratorTests.cs
@@ -64,8 +64,8 @@ namespace CGbR.Tests
                     AccessModifier = AccessModifier.Internal,
                     ValueType = value,
                     ElementType = value.ToString("G"),
+                    Attributes = DataMember
                 };
-                prop.Attributes.Add(new AttributeModel("DataMember"));
                 model.Properties.Add(prop);
             }
 
@@ -99,9 +99,9 @@ namespace CGbR.Tests
                     ElementType = value.ToString("G"),
                     Dimensions = 1,
                     IsCollection = true,
-                    CollectionType = colType
+                    CollectionType = colType,
+                    Attributes = DataMember
                 };
-                prop.Attributes.Add(new AttributeModel("DataMember"));
                 model.Properties.Add(prop);
             }
 
@@ -112,6 +112,96 @@ namespace CGbR.Tests
             // Assert by comparing generated fragment with test oracle
             var expected = File.ReadAllText(Path.Combine("Expected", $"{expectedCode}.txt"));
             Assert.AreEqual(expected, generated);
+        }
+
+        [TestCase(nameof(JsonSerializer), "JsonClasses")]
+        [TestCase(nameof(BinarySerializer), "BinaryClasses")]
+        [TestCase(nameof(Cloneable), "CloneClasses")]
+        public void ClassReference(string generator, string expectedCode)
+        {
+            // Arrange
+            var model = GenerateDataContract(AccessModifier.Internal);
+            model.Properties.Add(new PropertyModel("FixedSize")
+            {
+                ElementType = "FixedClass",
+                ValueType = ModelValueType.Class,
+                Attributes = DataMember
+            });
+            model.Properties.Add(new PropertyModel("FlexSize")
+            {
+                ElementType = "FlexClass",
+                ValueType = ModelValueType.Class,
+                Attributes = DataMember
+            });
+            model.Properties.Add(new PropertyModel("FixCollection")
+            {
+                IsCollection = true, 
+                ElementType = "FixedClass",
+                ValueType = ModelValueType.Class,
+                Attributes = DataMember,
+                Dimensions = 1,
+                CollectionType = "List"
+            });
+            model.Properties.Add(new PropertyModel("FlexCollection")
+            {
+                IsCollection = true,
+                ElementType = "FlexClass",
+                ValueType = ModelValueType.Class,
+                Attributes = DataMember,
+                Dimensions = 1,
+                CollectionType = "Array"
+            });
+            model.References.Add(GenerateSub(true));
+            model.References.Add(GenerateSub(false));
+
+            // Act
+            var gen = GetGenerator(generator);
+            var code = gen.Extend(model);
+
+            // Assert by comparing generated fragment with test oracle
+            var expected = File.ReadAllText(Path.Combine("Expected", $"{expectedCode}.txt"));
+            Assert.AreEqual(expected, code);
+        }
+
+        private static ClassModel GenerateSub(bool fixedSize)
+        {
+            var model = GenerateDataContract(AccessModifier.Public, fixedSize ? "FixedClass" : "FlexClass");
+
+            // Add a few fixed properties
+            model.Properties.Add(new PropertyModel("Integer")
+            {
+                ElementType = "int",
+                ValueType = ModelValueType.Int32,
+                Attributes = DataMember
+            });
+            model.Properties.Add(new PropertyModel("Double")
+            {
+                ElementType = "double",
+                ValueType = ModelValueType.Double,
+                Attributes = DataMember
+            });
+
+            // Add a collection and string for flex size only
+            if (fixedSize)
+                return model;
+
+            model.Properties.Add(new PropertyModel("Integers")
+            {
+                IsCollection = true,
+                ElementType = "int",
+                ValueType = ModelValueType.UInt32,
+                CollectionType = "Array",
+                Dimensions = 1,
+                Attributes = DataMember
+            });
+            model.Properties.Add(new PropertyModel("Text")
+            {
+                ElementType = "string",
+                ValueType = ModelValueType.String,
+                Attributes = DataMember
+            });
+
+            return model;
         }
 
         private static string CollectionType(ref int index)
@@ -128,17 +218,22 @@ namespace CGbR.Tests
         }
 
         /// <summary>
+        /// Data member definition for properties
+        /// </summary>
+        private static readonly List<AttributeModel> DataMember = new List<AttributeModel> {new AttributeModel("DataMember")};
+
+        /// <summary>
         /// Generate a class model to generate for
         /// </summary>
-        private static ClassModel GenerateDataContract(AccessModifier access)
+        private static ClassModel GenerateDataContract(AccessModifier access, string name = "Dummy")
         {
-            var model = new ClassModel("Dummy")
+            var model = new ClassModel(name)
             {
                 AccessModifier = access,
                 IsPartial = true,
+                Attributes = new List<AttributeModel> {new AttributeModel("DataContract")}, // Attributes for JSON and Binary
+                Interfaces = new[] {"ICloneable" } // Cloneable for CloneGenerator
             };
-            model.Attributes.Add(new AttributeModel("DataContract")); // Attributes for JSON and Binary
-            model.Interfaces = new[] { "ICloneable" }; // Cloneable for CloneGenerator
 
             return model;
         }
---
+++ CGbR.Tests/SizeCalculationTests.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace CGbR.GeneratorTests
+{
+    [TestFixture]
+    public class SizeCalculationTests
+    {
+        [TestCase(ModelValueType.Byte, 1)]
+        [TestCase(ModelValueType.Boolean, 1)]
+        [TestCase(ModelValueType.Char, 2)]
+        [TestCase(ModelValueType.Int16, 2)]
+        [TestCase(ModelValueType.UInt16, 2)]
+        [TestCase(ModelValueType.Int32,4)]
+        [TestCase(ModelValueType.UInt32, 4)]
+        [TestCase(ModelValueType.Single, 4)]
+        [TestCase(ModelValueType.Int64, 8)]
+        [TestCase(ModelValueType.UInt64, 8)]
+        [TestCase(ModelValueType.Double, 8)]
+        public void ValueTypeSize(ModelValueType type, int expectedSize)
+        {
+            // Arrange 
+            var model = new ClassModel("Dummy");
+            model.Properties.Add(new PropertyModel("Property")
+            {
+                ValueType = type,
+                ElementType = type.ToString("G"),
+                Attributes = new List<AttributeModel> { new AttributeModel("DataMember") }
+            });
+
+            // Act
+            var size = BinarySize.OfClass(model, new BinarySerializer());
+
+            // Assert
+            Assert.AreEqual(expectedSize, size);
+        }
+
+        [Test]
+        public void CollectionSizes()
+        {
+            // Arrange
+            var model = new ClassModel("Dummy");
+            model.Properties.Add(new PropertyModel("Dim1")
+            {
+                IsCollection = true,
+                CollectionType = "List",
+                Dimensions = 1,
+                Attributes = new List<AttributeModel> { new AttributeModel("DataMember") }
+            });
+            model.Properties.Add(new PropertyModel("Dim2")
+            {
+                IsCollection = true,
+                CollectionType = "Array",
+                Dimensions = 2,
+                Attributes = new List<AttributeModel> { new AttributeModel("DataMember") }
+            });
+
+            // Act
+            var size = BinarySize.OfClass(model, new BinarySerializer());
+
+            // Assert
+            Assert.AreEqual(6, size);
+        }
+
+        [TestCase(ModelValueType.Byte, 1)]
+        [TestCase(ModelValueType.Int16, 2)]
+        [TestCase(ModelValueType.Int32, 4)]
+        [TestCase(ModelValueType.Int64, 8)]
+        public void EnumSize(ModelValueType baseType, int expectedSize)
+        {
+            // Arrange
+            var model = new ClassModel("Dummy");
+            model.Properties.Add(new PropertyModel("Enum")
+            {
+                ElementType = "SomeEnum",
+                ValueType = ModelValueType.Class,
+                Attributes = new List<AttributeModel> { new AttributeModel("DataMember") }
+            });
+            model.References = new List<CodeElementModel> {new EnumModel("SomeEnum")
+            {
+                BaseType = baseType
+            }};
+
+            // Act
+            var size = BinarySize.OfClass(model, new BinarySerializer());
+
+            // Assert
+            Assert.AreEqual(expectedSize, size);
+        }
+    }
+}
